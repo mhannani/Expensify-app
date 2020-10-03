70a13e6247129dbc2bd4ce082cfe74a8eecf8f14
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-10-03 21:54:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Playground from './components/Playground.js'
+
+
+class App extends React.Component {
+
+    render(){
+        return (
+            <p> Expensify application came to life</p>
+        );
+    }
+
+}
+
+export default App;
